c7adde8993dbbc82d90db186d0e61028bbb9f63c
heromiya <heromiya@heromiya.net>
2015-11-25 11:07:12 +0900

    modified:   ../Data/targetExtents/target_tiles.sqlite 	modified:   Makefile 	modified:   extractImages.sh

--- heromiya/extractImages.sh
+++ heromiya/extractImages.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 mkdir -p sampleImages
+mkdir -p sampleImagesVRT
 export ZLEVEL=19
 export EPSG4326="+proj=longlat +datum=WGS84 +no_defs"
 export EPSG3857="+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs"
@@ -13,9 +14,18 @@ for QKEY in `cat completedSamples.lst`; do
     LATMAX=`echo $COORDS | cut -d '|' -f 4`
     iojs get.BingAerial.js $LONMIN $LATMIN $LONMAX $LATMAX $ZLEVEL > args1.lst
     cat args1.lst | xargs parallel --joblog log.txt --jobs 10% "./get.Bing.Aerial.Sub.sh" ::: 
+    
+    export MERGEDTILE=sampleImages/a${QKEY}-Z${ZLEVEL}.tif
+    export MERGEINPUT="`awk 'BEGIN{FS=\",\"}{printf(\"Bing/gtiff/19/a%s.tif \",$1)}' args1.lst`"
+    make $MERGEDTILE
 
-    gdalwarp `find -type f | grep ".*gtiff.*$QKEY.*tif"` sampleImages/a${QKEY}-Z${ZLEVEL}.tif
-#    gdal_rasterize 
+    eval `gdalinfo $MERGEDTILE | grep "Pixel Size" | sed 's/ //g;s/,/ /;s/-//'`
+    XMIN=`gdalinfo $MERGEDTILE | grep "Lower Left" | sed 's/Lower Left *(\{[0-9.-]*\}, \{[0-9.-]*\}) .*/\1/;`
+    YMIN=`gdalinfo $MERGEDTILE | grep "Lower Left" | sed 's/Lower Left *(\{[0-9.-]*\}, \{[0-9.-]*\}) .*/\2/;`
+    XMAX=`gdalinfo $MERGEDTILE | grep "Upper Right" | sed 's/Upper Right *(\{[0-9.-]*\}, \{[0-9.-]*\}) .*/\1/;`
+    YMAX=`gdalinfo $MERGEDTILE | grep "Upper Right" | sed 's/Upper Right *(\{[0-9.-]*\}, \{[0-9.-]*\}) .*/\2/;`
+    
+    gdal_rasterize -a flag -l working_polygon -tr ${PixelSize[0]} ${PixelSize[1]} -te $XMIN $YMIN $XMAX $YMAX working_polygon.sqlite sampleImages/r${QKEY}-Z${ZLEVEL}.tif
 done
 
 exit 0
