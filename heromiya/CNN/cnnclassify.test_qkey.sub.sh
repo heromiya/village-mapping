9f879ad4371c40216d5e062ac32f474d9dbc4186
heromiya <heromiya@heromiya.net>
2016-03-17 16:09:20 +0900

    modified:   CNN/Makefile 	modified:   CNN/cnnclassify.test_qkey.sub.sh 	modified:   Makefile

--- heromiya/CNN/cnnclassify.test_qkey.sub.sh
+++ heromiya/CNN/cnnclassify.test_qkey.sub.sh
@@ -5,6 +5,6 @@ export TILESVRT=tileList/$ZLEVEL/Z$ZLEVEL-$TEST_QKEY.vrt
 export TILES=tileList/$ZLEVEL/Z$ZLEVEL-$TEST_QKEY.lst
 export TRAINING_QKEY=$TEST_QKEY
 make -rR $TILES 
-cat $TILES | parallel --jobs 4 --joblog logs/cnnclassify.sub.nsample.conf.sh.$ZLEVEL.$NSAMPLE.$TEST_QKEY.`date +"%F_%T"` ./cnnclassify.sub.nsample.conf.sh :::
-
+cat $TILES | parallel --jobs 1 ./cnnclassify.sub.nsample.conf.sh  :::
+#--joblog logs/cnnclassify.sub.nsample.conf.sh.$ZLEVEL.$NSAMPLE.$TEST_QKEY.`date +"%F_%T"`
 exit 0
