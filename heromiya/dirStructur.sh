4fc7df5df422e46ec51ef9561480d4995099a894
Hiroyuki Miyazaki <heromiya@guam-iis.iis.u-tokyo.ac.jp>
2017-10-13 16:42:31 +0900

    modified:   Makefile 	modified:   dirStructur.sh

--- heromiya/dirStructur.sh
+++ heromiya/dirStructur.sh
@@ -2,13 +2,14 @@
 
 for TYPE in gtiff png; do
     cd GMap/$TYPE/18
-    find -maxdepth 1 -type f | sed 's/\.[0-9]*\.tif//g; s/\.\/Z18\.//g;' | uniq > dir.lst
+    find -maxdepth 1 -type f | sed 's/\.[0-9]*\.[a-z]..//g; s/\.\/Z18\.//g;' | uniq > dir.lst
 
     for DIR in `cat dir.lst`; do
 	echo $DIR
 	mkdir $DIR
-	mv ./Z18.$DIR.*.tif $DIR/
+	mv ./Z18.$DIR.* $DIR/
     done
+    cd ../../../
 done
 
 exit
