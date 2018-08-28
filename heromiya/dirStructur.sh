b05daa65d18bd2ea721f70b6a87c79b804970e59
Hiroyuki Miyazaki <heromiya@heromiya.net>
2018-08-28 14:32:40 +0900

    modified:   dirStructur.sh

--- heromiya/dirStructur.sh
+++ heromiya/dirStructur.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
-
+ZLEVEL=17
 for TYPE in gtiff png; do
-    cd GMap/$TYPE/18
-    find -maxdepth 1 -type f | sed 's/\.[0-9]*\.[a-z]..//g; s/\.\/Z18\.//g;' | uniq > dir.lst
+    cd GMap/$TYPE/$ZLEVEL
+    find -maxdepth 1 -type f | sed "s/\.[0-9]*\.[a-z]..//g; s/\.\/Z$ZLEVEL\.//g;" | uniq > /tmp/dir.lst
 
-    for DIR in `cat dir.lst`; do
-	echo $DIR
-	mkdir $DIR
-	mv ./Z18.$DIR.* $DIR/
+    for DIR in `cat /tmp/dir.lst`; do
+#	echo $DIR
+	mkdir -p $DIR
+	mv ./Z$ZLEVEL.$DIR.* $DIR/
     done
     cd ../../../
 done
