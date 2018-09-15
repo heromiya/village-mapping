b4e2cdd6a5711e768095b3540ad2ab83aaac1a62
Hiroyuki Miyazaki <heromiya@heromiya.net>
2018-09-15 15:59:32 +0900

    modified:   Makefile 	modified:   batch.getGoogleMaps.sh

--- heromiya/batch.getGoogleMaps.sh
+++ heromiya/batch.getGoogleMaps.sh
@@ -3,5 +3,18 @@
 #./getGoogleMaps.sh 105.6371 106.7222 16.0363 16.9907 17
 #./getGoogleMaps.sh 105.6371 106.7222 16.0363 16.9907 18
 
-./getGoogleMaps.sh 104.732625 106.798488 15.876399 17.113576 17
-./getGoogleMaps.sh 104.732625 106.798488 15.876399 17.113576 18
+# Savannakhet 
+#./getGoogleMaps.sh 104.732625 106.798488 15.876399 17.113576 17
+#./getGoogleMaps.sh 104.732625 106.798488 15.876399 17.113576 18
+
+# Mozanbique
+./getGoogleMaps.sh 30.2 41.0 -26.9 -10.4 17
+
+# Sri lanka
+./getGoogleMaps.sh 79.7 81.9 5.9 9.85 17
+
+# Mozanbique
+./getGoogleMaps.sh 30.2 41.0 -26.9 -10.4 18
+
+# Sri lanka
+./getGoogleMaps.sh 79.7 81.9 5.9 9.85 18
