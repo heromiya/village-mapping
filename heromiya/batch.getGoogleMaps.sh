3fabe6a0555f919e18939653c55dd6abd837a7e5
Hiroyuki Miyazaki <heromiya@guam-iis.iis.u-tokyo.ac.jp>
2018-10-13 14:39:44 +0900

    modified:   batch.getGoogleMaps.sh 	modified:   getGoogleMaps.sh

--- heromiya/batch.getGoogleMaps.sh
+++ heromiya/batch.getGoogleMaps.sh
@@ -9,20 +9,20 @@
 
 # Mozanbique
 #./getGoogleMaps.sh 30.2 41.0 -26.9 -10.4 17
-./getGoogleMaps.sh 30.2 35.6 -26.9 -18.65 17
-./getGoogleMaps.sh 35.6 41.0 -26.9 -18.65 17
-./getGoogleMaps.sh 30.2 35.6 -18.65 -10.4 17
-./getGoogleMaps.sh 35.6 41.0 -18.65 -10.4 17
+#./getGoogleMaps.sh 30.2 35.6 -26.9 -18.65 17
+#./getGoogleMaps.sh 35.6 41.0 -26.9 -18.65 17
+#./getGoogleMaps.sh 30.2 35.6 -18.65 -10.4 17
+#./getGoogleMaps.sh 35.6 41.0 -18.65 -10.4 17
 
 # Sri lanka
-./getGoogleMaps.sh 79.7 81.9 5.9 9.85 17
+#./getGoogleMaps.sh 79.7 81.9 5.9 9.85 17
 
 # Mozanbique
 #./getGoogleMaps.sh 30.2 41.0 -26.9 -10.4 18
-./getGoogleMaps.sh 30.2 35.6 -26.9 -18.65 18
-./getGoogleMaps.sh 35.6 41.0 -26.9 -18.65 18
-./getGoogleMaps.sh 30.2 35.6 -18.65 -10.4 18
-./getGoogleMaps.sh 35.6 41.0 -18.65 -10.4 18
+#./getGoogleMaps.sh 30.2 35.6 -26.9 -18.65 18
+#./getGoogleMaps.sh 35.6 41.0 -26.9 -18.65 18
+#./getGoogleMaps.sh 30.2 35.6 -18.65 -10.4 18
+#./getGoogleMaps.sh 35.6 41.0 -18.65 -10.4 18
 
 
 # Sri lanka
