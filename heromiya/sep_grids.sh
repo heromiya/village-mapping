15cdc8f5a170e1a729d2d4d72a0b09a8d45421ad
Hiroyuki Miyazaki <heromiya@heromiya.net>
2018-11-23 11:49:29 +0900

    modified:   getGoogleMaps.sh 	modified:   sep_grids.sh

--- heromiya/sep_grids.sh
+++ heromiya/sep_grids.sh
@@ -3,13 +3,13 @@
 TMPSQL=/dev/shm/tmp.sql
 rm -f $TMPSQL
 
-echo "DELETE FROM grid_17;" > $TMPSQL
-for QKEY in $(psql suvannaket -qAtc "select qkey from grid where validate = 2"); do
+echo "DELETE FROM grid_18;" > $TMPSQL
+for QKEY in $(psql suvannaket -qAtc "select qkey from grid_17 where validate = 2"); do
     for i in $(seq 1 4); do
 	for j in $(seq 1 4); do
 	    CHILDQKEY=${QKEY}${i}${j}
 	    GEOM=$(echo "var tilebelt=require('@mapbox/tilebelt');console.log(tilebelt.tileToBBOX(tilebelt.quadkeyToTile('$CHILDQKEY')))" | node | sed 's/\[/ST_MakeEnvelope(/; s/\]/, 4326)/')
-	    echo "INSERT INTO grid_17 (qkey, geom) VALUES ('$CHILDQKEY', $GEOM );" >> $TMPSQL 
+	    echo "INSERT INTO grid_18 (qkey, geom, validate) VALUES ('$CHILDQKEY', $GEOM, 2 );" >> $TMPSQL 
 	done
     done  
 done
