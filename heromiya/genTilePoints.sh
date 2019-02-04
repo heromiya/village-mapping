50e1bfcbbcb69b4bd29cd8e16fe37b682d11bd19
Hiroyuki Miyazaki <heromiya@hawaii.csis.u-tokyo.ac.jp>
2019-02-04 10:47:48 +0900

    modified:   heromiya/genTilePoints.sh

--- heromiya/genTilePoints.sh
+++ heromiya/genTilePoints.sh
@@ -4,7 +4,7 @@ export GADM_GID=$1
 export ZLEVEL=$2
 export WORKDIR=/dev/shm/tmp.$GADM_GID.$ZLEVEL
 export SQL=/dev/shm/tilePoint.insert.$GADM_GID.$ZLEVEL.sql
-
+mkdir -p $WORKDIR
 DB=/mnt/lv0/building_detection_outputs/tilePoints/tilePoint.$GADM_GID.Z$ZLEVEL.sqlite
 
 EXTENT=($(psql -h guam -d suvannaket -F " " -qAtc "SELECT ST_XMin(wkb_geometry), ST_XMax(wkb_geometry), ST_YMin(wkb_geometry), ST_YMax(wkb_geometry) FROM gadm36_level1 WHERE gid_1 = '$GADM_GID';"))
@@ -25,7 +25,7 @@ function tileCenter() {
 export -f tileCenter
 
 parallel -j0 --nice 10 --progress tileCenter {1} {2} ::: $(seq ${XYMIN[0]} ${XYMAX[0]}) ::: $(seq ${XYMIN[1]} ${XYMAX[1]})
-cat $WORKDIR/*.sql >> $SQL
+find $WORKDIR -type f | xargs cat >> $SQL
 
 rm -f $DB
 spatialite $DB < $SQL
