7626ebdbc2563c81db97f762d61cc1aced2cbc13
Hiroyuki Miyazaki <heromiya@hawaii.csis.u-tokyo.ac.jp>
2020-05-10 20:05:35 +0900

    modified:   heromiya/extractImages.d/extractImagesByGrid.sh

--- heromiya/extractImages.d/extractImagesByGrid.sh
+++ heromiya/extractImages.d/extractImagesByGrid.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+mkdir -p sampleImages/GMap
+
 #for ARGS in $(psql -h guam suvannaket -qAtc "select qkey,ST_XMIN(ST_Transform(geom,3857)),ST_YMIN(ST_Transform(geom,3857)),ST_XMAX(ST_Transform(geom,3857)),ST_YMAX(ST_Transform(geom,3857)),gadm0,x,y,z,validate from grid where validate = 1 OR validate = 2 limit 10"); do
 function getImage(){
     ARGS=$1
@@ -8,7 +10,7 @@ function getImage(){
     YMIN=$(echo $ARGS | cut -f 3 -d "|")
     XMAX=$(echo $ARGS | cut -f 4 -d "|")
     YMAX=$(echo $ARGS | cut -f 5 -d "|")
-    GADM0=$(echo $ARGS | cut -f 6 -d "|")
+    GADM=$(echo $ARGS | cut -f 6 -d "|")
     X=$(echo $ARGS | cut -f 7 -d "|")
     Y=$(echo $ARGS | cut -f 8 -d "|")
     Z=$(echo $ARGS | cut -f 9 -d "|")
@@ -17,11 +19,11 @@ function getImage(){
     WIDTH=$(perl  -e "print int(($XMAX - $XMIN)/0.29858214 + 0.5)")
 
     WORKDIR=$(mktemp -d)
-    wget -q -O $WORKDIR/i${QKEY}-${X}-${Y}-${Z}-${GADM0}-${V}.tif "http://hawaii.csis.u-tokyo.ac.jp:3857/service?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&LAYERS=Google%20Maps%20Satellite&STYLES=&WIDTH=${WIDTH}&HEIGHT=${HEIGHT}&FORMAT=image/geotiff&CRS=EPSG:3857&BBOX=${XMIN},${YMIN},${XMAX},${YMAX}"
-    rm -f  sampleImages/GMap/i${QKEY}-${X}-${Y}-${Z}-${GADM0}-${V}.tif
-    gdal_translate -q -of GTiff -co "compress=JPEG" -co "JPEG_QUALITY=75" $WORKDIR/i${QKEY}-${X}-${Y}-${Z}-${GADM0}-${V}.tif sampleImages/GMap/i${QKEY}-${X}-${Y}-${Z}-${GADM0}-${V}.tif
+    wget -q -O $WORKDIR/i${QKEY}-${X}-${Y}-${Z}-${GADM}-${V}.tif "http://hawaii.csis.u-tokyo.ac.jp:3857/service?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&LAYERS=Google%20Maps%20Satellite&STYLES=&WIDTH=${WIDTH}&HEIGHT=${HEIGHT}&FORMAT=image/geotiff&CRS=EPSG:3857&BBOX=${XMIN},${YMIN},${XMAX},${YMAX}"
+    rm -f  sampleImages/GMap/i${QKEY}-${X}-${Y}-${Z}-${GADM}-${V}.tif
+    gdal_translate -q -of GTiff -co "compress=JPEG" -co "JPEG_QUALITY=75" $WORKDIR/i${QKEY}-${X}-${Y}-${Z}-${GADM}-${V}.tif sampleImages/GMap/i${QKEY}-${X}-${Y}-${Z}-${GADM}-${V}.tif
     rm -rf $WORKDIR
 }
 export -f getImage
-parallel --progress getImage {} ::: $(psql -h guam suvannaket -qAtc "select qkey,ST_XMIN(ST_Transform(geom,3857)),ST_YMIN(ST_Transform(geom,3857)),ST_XMAX(ST_Transform(geom,3857)),ST_YMAX(ST_Transform(geom,3857)),gadm0,x,y,z,validate from grid where validate = 1 OR validate = 2")
+parallel --progress getImage {} ::: $(psql -h guam suvannaket -qAtc "select qkey,ST_XMIN(ST_Transform(geom,3857)),ST_YMIN(ST_Transform(geom,3857)),ST_XMAX(ST_Transform(geom,3857)),ST_YMAX(ST_Transform(geom,3857)),gadm1,x,y,z,validate from grid where validate = 1 OR validate = 2")
 #done
